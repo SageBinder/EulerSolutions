690169d2acc1d988e27deadb7a34566c37da6e8e
SageBinder <sagebin13@gmail.com>
2017-12-01 23:21:58 -0700

    Added solutions for Eulers 39 and 41

--- Euler39/src/com/sage/Main.java
+++ Euler39/src/com/sage/Main.java
@@ -3,11 +3,28 @@ package com.sage;
 public class Main {
 
     public static void main(String[] args) {
+        int greatestNumSolutions = 0;
+        int greatestP = 0;
+        for(int p = 1; p <= 1000; p++) {
+            int numSolutions = 0;
+            for(int a = 0; a < p; a++) {
+                for(int b = a; b < p - a; b++) {
+                    int c = p - a - b;
+                    if(isRightTriangleSolution(a, b, c)) {
+                        numSolutions++;
+                    }
+                }
+            }
 
+            if(numSolutions > greatestNumSolutions) {
+                greatestP = p;
+                greatestNumSolutions = numSolutions;
+            }
+        }
+        System.out.println(greatestP + ", " + greatestNumSolutions);
     }
 
     public static boolean isRightTriangleSolution(int a, int b, int c) {
-        if(a == 0 || b == 0 || c == 0) return false;
-        return Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2);
+        return a != 0 && b != 0 && c != 0 && Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2);
     }
 }
