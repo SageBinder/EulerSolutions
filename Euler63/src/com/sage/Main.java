e354cfa570213bdbf50d60ba840060c6b302985d
SageBinder <sagebin13@gmail.com>
2017-12-05 22:08:58 -0700

    Added solution for Euler63

---
+++ Euler63/src/com/sage/Main.java
@@ -0,0 +1,29 @@
+package com.sage;
+
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for(int i = 1; i < 10; i++) {
+            for(int n = 1; ; n++) {
+                BigInteger power = new BigInteger("" + i);
+                power = power.pow(n);
+                if(n > power.toString().length()) {
+                    break;
+                }
+                System.out.println("Checking " + i + " ^ " + n + ", which is " + power.toString() + ", numDigits is " + numDigits(power));
+                if(numDigits(power) == n) {
+                    sum++;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    public static int numDigits(BigInteger n) {
+        return n.toString().length();
+    }
+}
