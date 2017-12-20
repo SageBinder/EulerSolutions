c266a26824864b75f14bd5c4d8523973b30d9b8b
SageBinder <sagebin13@gmail.com>
2017-12-20 00:56:47 -0700

    Added fucking stupid shit for Euler206

--- Euler206/src/com/sage/Main.java
+++ Euler206/src/com/sage/Main.java
@@ -4,46 +4,33 @@ public class Main {
     public static void main(String[] args) {
         double answer = 0;
 
-        outerLoop:
-        for(int i = 0; i < 10; i++) {
-            for(int j = 0; j < 10; j++) {
-                for(int k = 0; k < 10; k++) {
-                    for(int l = 0; l < 10; l++) {
-                        for(int m = 0; m < 10; m++) {
-                            for(int n = 0; n < 10; n++) {
-                                for(int o = 0; o < 10; o++) {
-                                    for(int p = 0; p < 10; p++) {
-                                        double sqrt = Math.sqrt(Long.parseLong("1" + i +
-                                                "2" + j +
-                                                "3" + k +
-                                                "4" + l +
-                                                "5" + m +
-                                                "6" + n +
-                                                "7" + o +
-                                                "8" + p +
-                                                "9"));
-                                        System.out.println("Checking if " + "1" + i +
-                                                "2" + j +
-                                                "3" + k +
-                                                "4" + l +
-                                                "5" + m +
-                                                "6" + n +
-                                                "7" + o +
-                                                "8" + p +
-                                                "9 is a perfect square, sqrt is " + String.format("%.5f", sqrt));
+        for(long i = 999999999; i >= 0; i--) {
+            StringBuilder unknownDigitsSB = new StringBuilder(Long.toString(i));
+            unknownDigitsSB.reverse();
+            while(unknownDigitsSB.length() < 9) {
+                unknownDigitsSB.append("0");
+            }
+            unknownDigitsSB.reverse();
 
-                                        if(sqrt % 1.0 == 0) {
-                                            answer = sqrt;
-                                            break outerLoop;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+            StringBuilder squareSB = new StringBuilder();
+            for(int j = 0; j < 9; j++) {
+                squareSB.append(Integer.toString(j + 1)).append(unknownDigitsSB.charAt(j));
+            }
+            squareSB.append("9");
+
+            long square = Long.parseLong(squareSB.toString());
+            double sqrt = Math.sqrt(square);
+
+            System.out.println("Checking if " + square + ", " + squareSB.toString() + ", is a perfect square, sqrt is " + String.format("%.7f", sqrt));
+
+            if(sqrt % 1 == 0) {
+                System.out.println("Answer found, square is " + square + ", sqrt is " + String.format("%.7f", sqrt));
+                answer = sqrt;
+                break;
             }
         }
-        System.out.println(String.format("%.0f", answer));
+
+        // YOU KNOW WHAT THERE ARE FUCKING FLOATING POINT ERRORS AND SHIT IN THIS CODE BUT FUCK IT GETS THE RIGHT ANSWER
+        System.out.println(String.format("%.0f", answer) + ", " + String.format("%.0f", Math.pow(answer, 2)));
     }
 }
