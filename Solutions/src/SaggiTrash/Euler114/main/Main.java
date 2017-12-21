f77c876d0b60b87d24dfc319e4dee98381298df2
SageBinder <sagebin13@gmail.com>
2017-12-21 01:21:12 -0700

    Added solution for Euler114

---
+++ Solutions/src/SaggiTrash/Euler114/main/Main.java
@@ -0,0 +1,27 @@
+package SaggiTrash.Euler114.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(numCombinationsOneBlockApart(50));
+    }
+
+    private static long numCombinationsOneBlockApart(int numBlackBlocks) {
+        // This bullshit is to account for the case where no tiles are replaced
+        return numCombinationsOneBlockApartRecurse(numBlackBlocks) + 1;
+    }
+
+    private static long numCombinationsOneBlockApartRecurse(int numBlackBlocks) {
+        long answer = 0;
+
+        for(int oblongTileLength = 3; oblongTileLength <= numBlackBlocks; oblongTileLength++) {
+            answer += numBlackBlocks - oblongTileLength + 1;
+
+            for(int i = numBlackBlocks - oblongTileLength - 1; i > 0; i--) {
+                answer += numCombinationsOneBlockApartRecurse(i);
+            }
+        }
+
+        return answer;
+    }
+}
