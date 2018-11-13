2cba5a6489b763cbe1bd3d0e9fb3cb973f4b64d2
SageBinder <sagebin13@gmail.com>
2018-11-12 20:30:31 -0700

    Added solution for Euler113

---
+++ Solutions/src/SaggiTrash/Euler108/main/Main.java
@@ -0,0 +1,5 @@
+package SaggiTrash.Euler108.main;
+
+public class Main {
+
+}
---
+++ Solutions/src/SaggiTrash/Euler205/main/Main.java
@@ -0,0 +1,8 @@
+package SaggiTrash.Euler205.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
