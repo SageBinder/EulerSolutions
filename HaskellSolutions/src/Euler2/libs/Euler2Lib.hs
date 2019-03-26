138c5b6940ca006c2150cf310226feb26c43da16
SageBinder <sagebin13@gmail.com>
2019-03-26 16:24:52 -0700

    Removed some parenthesis cause my IDE keeps going "ReeEEEeEeEeEEeEE unnecessary brackets"

--- HaskellSolutions/src/Euler2/libs/Euler2Lib.hs
+++ HaskellSolutions/src/Euler2/libs/Euler2Lib.hs
@@ -1,2 +1,2 @@
 module Euler2Lib(fibSum) where
-  fibSum a b max = if (a + b) > max then 0 else fibSum b (a + b) max + if (a + b) `mod` 2 == 0 then (a + b) else 0
+  fibSum a b max = if (a + b) > max then 0 else fibSum b (a + b) max + if (a + b) `mod` 2 == 0 then a + b else 0
